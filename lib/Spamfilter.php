b0cc6f5911eb37ef5cc87a5d621185783009fad7
Bram Matthys <syzop@vulnscan.org>
2023-01-06 19:47:33 +0100

    Add Spamfilter support

---
+++ lib/Spamfilter.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace UnrealIRCd;
+
+use Exception;
+use stdClass;
+
+class Spamfilter
+{
+
+    public Connection $connection;
+
+    public function __construct(Connection $conn)
+    {
+        $this->connection = $conn;
+    }
+
+    /**
+     * Add a spamfilter.
+     *
+     * @return stdClass
+     * @throws Exception
+     */
+    public function add(string $name, string $match_type, string $spamfilter_targets, string $ban_action, string $ban_duration, string $reason): stdClass
+    {
+        return $this->connection->query('spamfilter.add', [
+            'name' => $name,
+            'match_type' => $match_type,
+            'spamfilter_targets' => $spamfilter_targets,
+            'ban_action' => $ban_action,
+            'ban_duration' => $ban_duration,
+            'reason' => $reason,
+        ]);
+    }
+
+    /**
+     * Delete a spamfilter.
+     *
+     * @return stdClass
+     * @throws Exception
+     */
+    public function delete(string $name, string $match_type, string $spamfilter_targets, string $ban_action): stdClass
+    {
+        return $this->connection->query('spamfilter.del', [
+            'name' => $name,
+            'match_type' => $match_type,
+            'spamfilter_targets' => $spamfilter_targets,
+            'ban_action' => $ban_action,
+        ]);
+    }
+
+    /**
+     * Return a list of all spamfilters.
+     *
+     * @return stdClass
+     * @throws Exception
+     */
+    public function getAll(): stdClass
+    {
+        $response = $this->connection->query('spamfilter.list');
+
+        if (!is_bool($response)) {
+            return $response;
+        }
+
+        throw new Exception('Invalid JSON Response from UnrealIRCd RPC.');
+    }
+
+    /**
+     * Get a specific spamfilter.
+     *
+     * @return stdClass
+     * @throws Exception
+     */
+    public function get(string $name, string $match_type, string $spamfilter_targets, string $ban_action): stdClass
+    {
+        $response = $this->connection->query('spamfilter.get', [
+            'name' => $name,
+            'match_type' => $match_type,
+            'spamfilter_targets' => $spamfilter_targets,
+            'ban_action' => $ban_action,
+        ]);
+
+        if (!is_bool($response)) {
+            return $response;
+        }
+
+        throw new Exception('Invalid JSON Response from UnrealIRCd RPC.');
+    }
+}
